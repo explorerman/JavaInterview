4ecd23d39404fdd7f465214374578fcba9fd8beb
lijie <648015616@qq.com>
2020-03-18 22:41:07 +0800

    修改swordoffer的编码

--- swordOffer/src/arrayOperation/Find.java
+++ swordOffer/src/arrayOperation/Find.java
@@ -3,18 +3,18 @@ package arrayOperation;
 public class Find {
 
 	/**
-	 * 二维数组中的查找
+	 * ????????械????
 	 * @param array
 	 * @param target
 	 * @return
 	 */
-	//二分法,典型的二分法模板
+	//?????,????????????
 	public boolean Find(int [][]array,int target){
 		int rowLength = array.length;
 		for(int i = 0; i < rowLength; i++){
 			int low = 0;
 			int high = array[i].length - 1;
-			while(low <= high){   //注意循环条件是low <= high 
+			while(low <= high){   //????????????low <= high 
 				int mid = low + (high - low)/2;
 				if(target > array[i][mid]){
 					low = mid + 1;
@@ -30,8 +30,8 @@ public class Find {
 	
 	public boolean Find2(int [][]array,int target){
 		int m,n,x,y;
-		m = array.length; //行数
-		n = array[0].length; //列数
+		m = array.length; //????
+		n = array[0].length; //????
 		x = m-1;
 		y = 0;
 		while(x>=0 && y<=n-1){
