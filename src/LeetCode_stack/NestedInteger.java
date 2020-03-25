48ee352c6258fed227965b17c15e3058985582b7
JIEE_Shine <648015616@qq.com>
2020-03-25 17:59:25 +0800

    增加341

---
+++ src/LeetCode_stack/NestedInteger.java
@@ -0,0 +1,10 @@
+package LeetCode_stack;
+
+import java.util.List;
+
+public interface NestedInteger {
+
+    public boolean isInteger();
+    public Integer getInteger();
+    public List<NestedInteger> getList();
+}
--- src/LeetCode_stack/number341.java
+++ src/LeetCode_stack/number341.java
@@ -2,20 +2,38 @@ package LeetCode_stack;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.Stack;
 
 //扁平化嵌套列表迭代器
+//设计类，如何使用接口来写代码
+//1.新建接口，接口中新增方法
+//2.别的类中把接口当做类来使用，不用管接口中的方法是否实现。
+//3.最后补全接口实现类
 public class number341 implements Iterator<Integer> {
-    public number341(List<number341> nestedList) {
-
+    private Stack<NestedInteger> stack = new Stack<>();
+    public number341(List<NestedInteger> nestedList) {
+        for (int i = nestedList.size() - 1; i >= 0 ; i--) {
+            stack.push(nestedList.get(i));
+        }
     }
 
     @Override
     public Integer next() {
-        return 0;
+        NestedInteger t = stack.peek();
+        stack.pop();
+        return t.getInteger();
     }
 
     @Override
     public boolean hasNext() {
+        while (!stack.isEmpty()){
+            NestedInteger t = stack.peek();
+            if(t.isInteger()) return true;
+            stack.pop();
+            for (int i = t.getList().size() - 1; i >=0 ; i--) {
+                stack.push(t.getList().get(i));
+            }
+        }
         return false;
     }
 }
