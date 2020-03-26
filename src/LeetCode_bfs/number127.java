d02e09d60a8ccbefa90889eaa261c4640d04c4af
lijie <648015616@qq.com>
2020-03-26 23:52:14 +0800

    127解答

--- src/LeetCode_bfs/number127.java
+++ src/LeetCode_bfs/number127.java
@@ -1,10 +1,72 @@
 package LeetCode_bfs;
 
-import java.util.List;
+import java.util.*;
 
 //单词接龙
 public class number127 {
+//    beginWord = "hit",
+//    endWord = "cog",
+//    wordList = ["hot","dot","dog","lot","log","cog"]
+
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        if (!wordList.contains(endWord)) {
+            return 0;
+        }
+        Set<String> visited = new HashSet<>();
+        Queue<String> queue = new LinkedList<>();
+        queue.offer(beginWord);
+        visited.add(beginWord);
+        int count = 0;
+        while (queue.size() > 0) {
+            int size = queue.size();
+            ++count;
+            for (int i = 0; i < size; ++i) {
+                String start = queue.poll();
+                for (String s : wordList) {
+                    // 已经遍历的不再重复遍历
+                    if (visited.contains(s)) {
+                        continue;
+                    }
+                    // 不能转换的直接跳过
+                    if (!canConvert(start, s)) {
+                        continue;
+                    }
+                    // 用于调试
+                     System.out.println(count + ": " + start + "->" + s);
+                    // 可以转换，并且能转换成endWord，则返回count
+                    if (s.equals(endWord)) {
+                        return count + 1;
+                    }
+                    // 保存访问过的单词，同时把单词放进队列，用于下一层的访问
+                    visited.add(s);
+                    queue.offer(s);
+                }
+            }
+        }
         return 0;
+    }
+
+    public boolean canConvert(String s1, String s2) {
+        if (s1.length() != s2.length()) return false;
+        int count = 0;
+        for (int i = 0; i < s1.length(); ++i) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                ++count;
+                if (count > 1) {
+                    return false;
+                }
+            }
+        }
+        return count == 1;
+    }
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("hot");
+        list.add("dot");
+        list.add("lot");
+        list.add("log");
+        list.add("cog");
+        new number127().ladderLength("hit","cog",list);
     }
 }
