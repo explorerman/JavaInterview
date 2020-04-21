29c607e926cf909af174b0326e8d1c8e20bf0c26
lijie <648015616@qq.com>
2020-04-21 08:24:19 +0800

    新增juc包下的测试类

--- src/futureTest/FutureData.java
+++ src/futureTest/FutureData.java
@@ -5,6 +5,7 @@ public class FutureData implements Data {
 	protected boolean isReady = false;
 
 	public synchronized void setRealData(RealData realData) {
+		System.out.println("set中的is————"+isReady);
 		if(isReady){
 			return;
 		}
@@ -16,6 +17,7 @@ public class FutureData implements Data {
 	
 	@Override
 	public synchronized String getResult() {
+		System.out.println("使用了future的result__" + isReady);
 		while (!isReady) {
 			try {
 				System.out.println("isReady没有准备好" + isReady+"-需要准备好数据才行");
