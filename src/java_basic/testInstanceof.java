ff80063cdada0731914bae498fd4dcb7332b946c
lijie <648015616@qq.com>
2020-05-10 18:44:21 +0800

    新增了lc的面试题

---
+++ src/java_basic/testInstanceof.java
@@ -0,0 +1,13 @@
+package java_basic;
+
+import leetcode_tree.Person;
+
+public class testInstanceof {
+    public static void main(String[] args) {
+
+        StringBuilder sb = new StringBuilder();
+        if(sb instanceof Object){
+            System.out.println(true);
+        }
+    }
+}
