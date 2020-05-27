73d862b16a8257a33002747dc968864a34886a5c
lijie <648015616@qq.com>
2020-05-27 08:23:53 +0800

    新增了lc的面试题

--- src/JUC_pv/MulitComputerNumber.java
+++ src/JUC_pv/MulitComputerNumber.java
@@ -8,21 +8,27 @@ import java.util.concurrent.FutureTask;
  * 需求：使用countdownLatch倒计时法+future实现传入不同的数，计算不同的结果
  */
 public class MulitComputerNumber {
-    public static void main(String[] args) throws InterruptedException, ExecutionException {
-        final int total = 100;
+    public static void main(String[] args){
+         final int total = 100;
         CountDownLatch countDownLatch = new CountDownLatch(total);
-        for (int i = 0; i < total; i++) {
-            FutureTask<Boolean> futureTask = new FutureTask<Boolean>(new FutureTaskComputer(i));
-            ThreadPoolExecutorExapmle.threadPoolExecutor1.execute(futureTask);
-            if(futureTask.get()){
-                System.out.println("发生了错误");
-            }else{
-                System.out.println("一个任务完成");
+        try {
+            for (int i = 0; i < total; i++) {
+                FutureTask<Boolean> futureTask = new FutureTask<Boolean>(new FutureTaskComputer(i));
+                ThreadPoolExecutorExapmle.threadPoolExecutor1.execute(futureTask);
+                if(futureTask.get()){
+                    System.out.println("发生了错误");
+                    throw new Exception("错误");
+                }else{
+                    System.out.println("一个任务完成");
+                }
+                countDownLatch.countDown();
             }
-            countDownLatch.countDown();
+            countDownLatch.await();
+            System.out.println("success");
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            ThreadPoolExecutorExapmle.threadPoolExecutor1.shutdown();
         }
-        countDownLatch.await();
-        System.out.println("success");
-        ThreadPoolExecutorExapmle.threadPoolExecutor1.shutdown();
     }
 }
