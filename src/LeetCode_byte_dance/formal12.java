42650f5cf8406b0573dee7d04ab2256609621631
lijie <648015616@qq.com>
2020-06-01 08:18:00 +0800

    新增了lc的面试题

--- src/LeetCode_byte_dance/formal12.java
+++ src/LeetCode_byte_dance/formal12.java
@@ -2,4 +2,5 @@ package LeetCode_byte_dance;
 
 public class formal12 {
     //编程是对一个链表，每k个节点进行一次翻转
+
 }
---
+++ src/leetcode_list/number25.java
@@ -0,0 +1,9 @@
+package leetcode_list;
+
+//K个一维翻转链表
+public class number25 {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode dummy = new ListNode(-1);
+        return dummy.next;
+    }
+}
