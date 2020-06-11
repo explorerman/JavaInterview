8e19d13558195e2014480c41fafd24989f135243
lijie <648015616@qq.com>
2020-06-11 08:15:06 +0800

    新增了316,321，是402，移除K个元素的变种题

---
+++ src/LeetCode_byte_dance/number316.java
@@ -0,0 +1,9 @@
+package LeetCode_byte_dance;
+
+//去除重复字母
+//使用单调栈
+public class number316 {
+    public String removeDuplicateLetters(String s) {
+        return "";
+    }
+}
---
+++ src/LeetCode_byte_dance/number321.java
@@ -0,0 +1,13 @@
+package LeetCode_byte_dance;
+
+//拼接最大数
+//单调栈
+public class number321 {
+    /**
+     * 1.数组1，2找出递减序列，找出递减序列开始时，最大的一个数当高位
+     * 2.依次找
+     */
+    public int[] maxNumber(int[] nums1, int[] nums2, int k) {
+        return null;
+    }
+}
