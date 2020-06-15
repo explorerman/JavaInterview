7ffc4acc40373c14e6d5992408df192237bfc694
lijie <648015616@qq.com>
2020-06-15 08:14:33 +0800

    新增了头条面试题

---
+++ src/LeetCode_review_2/number322_1.java
@@ -0,0 +1,30 @@
+package LeetCode_review_2;
+
+//零钱兑换
+public class number322_1 {
+    int res = Integer.MAX_VALUE;
+    public int coinChange(int[] coins, int amount) {
+        if (coins.length == 0) {
+            return -1;
+        }
+        int[] memo = new int[amount];
+        DFS(coins, amount, 0);
+        if(res == Integer.MAX_VALUE){
+            return -1;
+        }
+            return res;
+
+    }
+
+    private void DFS(int[] coins, int amount, int count) {
+        if(amount < 0) {
+            return;
+        }
+        if(amount == 0){
+            res = Math.min(res, count);
+        }
+        for (int coin : coins){
+            DFS(coins, amount - coin, count + 1);
+        }
+    }
+}
