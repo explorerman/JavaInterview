c88e5c8415c81afcf9bb6246996a659ba5de6310
lijie <648015616@qq.com>
2020-06-28 08:57:21 +0800

    新增了lcs

--- src/LeetCode_dp/number1143_2.java
+++ src/LeetCode_dp/number1143_2.java
@@ -8,12 +8,15 @@ public class number1143_2 {
         if (text1 == null || text2 == null || text1.length() == 0 || text2.length() == 0) return 0;
         int len1 = text1.length();
         int len2 = text2.length();
+        //dp方程：初始化为长度+1，是因为右下角的值要与坐上角比较，如果初始化为长度时，在第一步的时候，会发生数组越界
         int[][] dp = new int[len1 + 1][len2 + 1];
         for (int i = 1; i <= len1 ; i++) {
             for (int j = 1; j <= len2; j++) {
+                //dp方程：如果两个字符串字符相同，当前位置的值等于左上角的值+1
                 if(text1.charAt(i - 1) == text2.charAt(j - 1)){
                     dp[i][j] = dp[i - 1][j - 1] + 1;
                 }else {
+                    //
                     dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
                 }
             }
