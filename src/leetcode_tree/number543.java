8929a764106ae4eccfdd58820586b802e635b1bd
lijie <648015616@qq.com>
2020-07-27 08:19:56 +0800

    新增了基本知识点

--- src/leetcode_tree/number543.java
+++ src/leetcode_tree/number543.java
@@ -12,4 +12,20 @@ package leetcode_tree;
  * @history 修订历史（历次修订内容、修订人、修订时间等）
  */
 public class number543 {
+    int max = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        if(root == null) return 0;
+        dfs(root);
+        return max;
+    }
+
+    private int dfs(TreeNode root) {
+        if(root == null) return 0;
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+        //将每个节点最大直径(左子树深度+右子树深度)当前最大值比较并取大者
+        max = Math.max(max, left + right);
+        //返回节点深度
+        return Math.max(left, right) + 1;
+    }
 }
