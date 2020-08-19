16cc408eb7ce0047c6a2a31ae5669f307d60c6fb
lijie <648015616@qq.com>
2020-08-19 08:18:47 +0800

    新增了循环队列

--- src/java_basic/MyCircularQueue.java
+++ src/java_basic/MyCircularQueue.java
@@ -7,7 +7,7 @@ package java_basic;
  * @package
  * @file ${fileName.java} number622
  * @createTime: 创建时间: 2020/6/9 10:05
- * @title 标题: 用数组实现一个循环队列
+ * @title 标题: 用数组实现一个循环队列    设计一个循环队列
  * 用数组实现一个循环队列，进阶:在上面的基础上实现一个无锁的支持并发的队列
  * @module 模块: 模块名称
  * @reviewer 审核人
@@ -18,8 +18,12 @@ public class MyCircularQueue {
     //队列长度
     int capacity;
     //队列头指针:始终指向当前元素的队首位置
+    // 在 front 出队，故设计在数组的头部，方便删除元素
+    // 删除元素的时候，只索引 +1（注意取模）
     int front;
     //队列尾指针：始终指向当前队尾元素的下一个位置，也就是下一个入队元素的位置
+    // 在 rear 入队，故设计在数组的尾部，方便插入元素
+    // 插入元素的时候，先赋值，后索引 +1（注意取模）
     int rear;
 
     /**
@@ -90,6 +94,8 @@ public class MyCircularQueue {
      * Checks whether the circular queue is full or not.
      */
     public boolean isFull() {
+        // 注意：这是这个经典设计的原因
+        //使用循环数组实现的队列，实际存储的元素比数组的长度至少少一个单位，我们认为是浪费一个单位是为了保证，队列为空，队列为满的判断条件不冲突
         if((rear + 1) % capacity == front){
             return true;
         }else {
