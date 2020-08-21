ed022a878ed4bb37219a2552b0163e0292458d2e
lijie <648015616@qq.com>
2020-08-21 08:20:06 +0800

    新增了回溯中的排列，组合问题

--- src/leetcode_backtracking/conbination_1.java
+++ src/leetcode_backtracking/conbination_1.java
@@ -2,12 +2,26 @@ package leetcode_backtracking;
 
 //蚂蚁金服二面算法题：3.输入一个字符串，输出该字符串中字符的所有组合。举个例子，如果输入abc，它的组合有a、b、c、ab、ac、bc、abc
 public class conbination_1 {
+    public static void combine(String str) {
+        if (str == null)
+            return;
+        int length = str.length();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < length; i++) {
+            //i即为字符串的组合中最长字符的长度
+            combination(str, 0, i, sb);
+        }
+    }
+    //index控制字符串中的索引，number用来控制组合字符串的长度即a,ab,abc等
     public static void combination(String str, int index, int number,
                                    StringBuilder sb) {
+        //回溯的出口
         if (number == -1) {
             System.out.println(sb.toString());
             return;
         }
+        //控制index的边界
         if (index == str.length())
             return;
 
@@ -21,19 +35,9 @@ public class conbination_1 {
 
     }
 
-    public static void combine(String str) {
-        if (str == null)
-            return;
-        int length = str.length();
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < length; i++) {
-            combination(str, 0, i, sb);
-        }
-    }
 
     public static void main(String[] args) {
-        combine("abcd");
+        combine("abc");
 
     }
 }
