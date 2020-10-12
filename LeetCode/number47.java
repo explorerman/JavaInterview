9045f4bc86cb8584349ba99433ade7c39037d64e
JIEE_Shine <648015616@qq.com>
2020-10-12 20:53:56 +0800

    增加47

--- LeetCode/number47.java
+++ LeetCode/number47.java
@@ -6,26 +6,28 @@ public class number47 {
     public List<List<Integer>> permuteUnique(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         if(nums == null || nums.length == 0) return res;
-        //使用visited可以防止有重复数字的全排列
-        boolean[] visited = new boolean[nums.length];
+        //使用visited可以防止有重复数字的全排列，0是未访问，1是访问
+        int[] visited = new int[nums.length];
         permuteDFS(res,new ArrayList<Integer>(),nums, visited);
         return res;
     }
-    private void permuteDFS(List<List<Integer>> result, ArrayList<Integer> levelList, int[] nums, boolean[] visited){
+    private void permuteDFS(List<List<Integer>> result, ArrayList<Integer> levelList, int[] nums, int[] visited){
         if(levelList.size() == nums.length){
             result.add(new ArrayList<>(levelList));
             return;
         }
         for(int i = 0; i < nums.length; i++){
-            //排除不合法的选择
-            if (visited[i] == true) continue;
-            visited[i] = true;
+            //已访问过，跳出循环
+            if (visited[i] == 1) continue;
+            //如果数组相连元素相等，没有先访问后面的元素，就不会存在重复
+            if(i>0 && nums[i] == nums[i - 1] && visited[i - 1] == 0) continue;
+            visited[i] = 1;
             //做选择
             levelList.add(nums[i]);
             //进入下一层决策树
             permuteDFS(result,levelList,nums, visited);
             //撤销
-            visited[i] = false;
+            visited[i] = 0;
             //取消选择
             levelList.remove(levelList.size() - 1);
         }
